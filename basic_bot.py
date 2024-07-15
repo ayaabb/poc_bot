2b9fc56e4322e7b039689f092a630272aa655af8
ayaabb <ayaabbas011@gmail.com>
2024-07-15 23:06:31 +0300

    added generate command

--- basic_bot.py
+++ basic_bot.py
@@ -16,14 +16,23 @@ async def test(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = requests.get(f'{os.getenv("server_addr")}/test')
     await update.message.reply_text(message.json()["Message"])
 
+async def store_random_num(update:Update,context:ContextTypes.DEFAULT_TYPE):
+    response = requests.get(f'{os.getenv("server_addr")}/generate')
+    if response.status_code == 200:
+        data = response.json()
+        await update.message.reply_text(f"Number generated and saved: {data['number']}")
+    else:
+        await update.message.reply_text(response.content.decode())
 
 
 def main():
     application = Application.builder().token(os.getenv("token")).build()
     start_handler = CommandHandler('start', start)
-    info_handler = CommandHandler('test', test)
-    application.add_handler(info_handler)
+    test_handler = CommandHandler('test', test)
+    generate_handler = CommandHandler('generate', store_random_num)
+    application.add_handler(test_handler)
     application.add_handler(start_handler)
+    application.add_handler(generate_handler)
     application.run_polling()
 
 
